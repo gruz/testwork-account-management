7ab6db872c3aad0e3de8ab2af4901d5f92519b36
Gruz <arygroup@gmail.com>
2021-01-08 22:24:40 +0200

    Adds routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function () {
+    Route::get('/ping', function () {
+        return response()->json(['Version 1']);
+    });
+    Route::post('/amount', [TransactionController::class, 'create']);
+    Route::get('/transaction/{transaction_id}', [TransactionController::class, 'getByID']);
+    Route::get('/balance/{account_id}', [AccountController::class, 'getBalance']);
+    Route::get('/max_transaction_volume', [AccountController::class, 'getMaxTransactionVolume']);
 });
